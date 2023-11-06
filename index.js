a6b59cf2eb13230c452b6d069ddff49f9fe6c1a0
shiyam-sarker10 <kaneki438500929@gmail.com>
2023-11-07 03:02:18 +0600

    set up mongoDb

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
+
+
+
 // middleware
 app.use(
   cors({
@@ -16,9 +19,15 @@ app.use(
 );
 app.use(express.json());
 app.use(cookieParser());
-// ${process.env.DB_USER}:${process.env.DB_PASS}
+
+
+
+
+
 const uri =
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.obcasl9.mongodb.net/?retryWrites=true&w=majority`;
+
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,10 +37,17 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+
+
+
 const logger = (req, res, next) => {
   console.log("we are at", req.host, req.originalUrl);
   next();
 };
+
+
+
 const verifyToken = (req, res, next) => {
   const token = req.cookies?.token;
 
@@ -47,17 +63,28 @@ const verifyToken = (req, res, next) => {
   });
 };
 
+
+
+
 async function run() {
   try {
+
+
+
+
     // Connect the client to the server	(optional starting in v4.7)
 
     await client.connect();
 
     const roomCollection = client.db("assignment11DB").collection("rooms");
     const sitCollection = client.db("assignment11DB").collection("roomSit");
+    const BookedCollection = client.db("assignment11DB").collection("booked");
 
 
     // auth related api
+
+
+
 
     app.post("/jwt", logger, (req, res) => {
       const user = req.body;
@@ -74,15 +101,80 @@ async function run() {
 
     // main api 
 
+
+
     app.get("/rooms", async (req, res) => {
       const rooms = await roomCollection.find({}).toArray();
       res.send(rooms);
     })
+
+
+
+    app.get("/roomSit", async (req, res) => {
+      const roomSit = await sitCollection.find({}).toArray();
+      res.send(roomSit);
+    })
+
+
+
     app.get("/roomSit/:id", async (req, res) => {
       const commonId = req.params.id;
       const rooms = await sitCollection.find({ commonId: commonId }).toArray();
       res.send(rooms);
     })
+    app.put("/roomIdSit/:id", async (req, res) => {
+      const Id = req.params.id;
+      const data = req.body;
+      const query = { _id: new ObjectId(Id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          available: data.available,
+        },
+      };
+      const result = await sitCollection.updateOne(query, updateDoc, options);
+
+      res.send(result);
+    });
+
+
+
+    app.get("/rooms/:id", async (req, res) => {
+      const Id = req.params.id;
+      const query = { roomId: Id };
+      const result = await roomCollection.findOne(query);
+      res.send(result);
+    });
+
+
+    app.get("/roomIdSit/:id", async (req, res) => {
+      const Id = req.params.id;
+      const query = { _id: new ObjectId(Id) };
+      const result = await sitCollection.findOne(query);
+      res.send(result);
+    });
+
+
+
+    app.post('/myBooking', async (req,res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await BookedCollection.insertOne(booking);
+      res.send(result);
+    })
+    app.get("/myBooking/:email", async (req, res) => {
+      const newEmail = req.params.email
+      const roomSit = await BookedCollection.find({
+        email: newEmail,
+      }).toArray();
+      res.send(roomSit);
+    });
+
+
+
+
+
+    
     
 
    
